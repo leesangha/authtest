112b8efd1e0578d26332e6cc082f9f79a252e01c
leesangha <sanghalee1@naver.com>
2023-07-12 14:23:36 +0900

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,27 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const [session] = useSession();
+  const { data: session } = useSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {!session && (
         <>
           Not signed in <br />
-          <button onClick={() => signIn("apple")}>Sign in</button>
+          <button
+            onClick={() => {
+              // console.log(context);
+              signIn("apple");
+            }}
+          >
+            Sign in
+          </button>
         </>
       )}
+
       {session && (
         <>
-          Signed in as {session.user.email} <br />
+          Signed in as {session?.user?.email} <br />
           <button onClick={() => signOut()}>Sign out</button>
         </>
       )}
