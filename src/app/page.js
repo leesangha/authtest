8690fe305e4fe6aa61dc076a85490eda1ccb6441
leesangha <sanghalee1@naver.com>
2023-07-12 16:20:52 +0900

    session

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-
+  console.log(session);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {!session && (
