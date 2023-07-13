e62822ec8cd3ba2e99cf103abc7eb4e1eb36ed2c
leesangha <sanghalee1@naver.com>
2023-07-13 18:15:13 +0900

    session 확인

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,15 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
 
-  useEffect(() => {
-    console.log(session);
-    if (session.provider === "apple")
-      fetch("https://3.35.97.160:5000/accounts/v1/rest-auth/apple", {
-        method: "post",
-        data: {
-          access_token: session.token.accessToken,
-          id_token: session.token.id_token,
-        },
-      }).then((res) => console.log(res));
+  const dt = useMemo(() => {
+    return session;
   }, [session]);
+  console.log(dt);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
