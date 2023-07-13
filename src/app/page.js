09751367ce5f5fe591d9fa7b3cfc58bda64daefd
leesangha <sanghalee1@naver.com>
2023-07-13 18:00:27 +0900

    tet

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>hihi</div>
+      <div>infludeo</div>
       {!session && (
         <>
           <button
